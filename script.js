940a9521d2a76be0edc3302fff2e3dbe4e875c38
nosleoj0022 <renangabardoazevedo0022@gmail.com>
2025-07-07 01:48:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const paragrafo = document.getElementById("mensagem");
+    paragrafo.textContent = "Sempre vai ter um jeito mais facil de se fazer algo, então ao invez de se estressar tenha outra perspectiva!";
+  }
